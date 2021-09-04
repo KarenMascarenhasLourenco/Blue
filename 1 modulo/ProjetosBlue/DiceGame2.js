cca2faa007c0ce30746533256a39b49105eeecc4
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2021-09-04 20:05:35 -0300

    atualização de node
    Atualização do readme e do projeto dice

--- 1 modulo/ProjetosBlue/DiceGame2.js
+++ 1 modulo/ProjetosBlue/DiceGame2.js
@@ -1,11 +1,5 @@
-/*  Perguntar quantas rodadas você quer fazer; (1,0 ponto) OK
-• Perguntar quantos jogadores vão jogar; (1,5 pontos)OK
-• Criar um objeto pra cada jogador com nome e número tirado; (1,5 pontos) OK
-• Guarda todos os objetos em uma lista; (2,0 pontos)OK
-• Ordenar esses objetos, sabendo que o vencedor tirou o maior número
-no dado. (2,0 pontos)OK
-• Mostrar no final qual jogador ganhou mais rodadas e foi o grande
-campeão. (2,0 pontos)*/
+//Autora: Karen Mascarenhas Lourenço
+// Projeto 03 Dice Game
 const prompt = require("prompt-sync")();
 let valorDados = [1, 2, 3, 4, 5, 6];
 var lista = [];
@@ -31,7 +25,7 @@ do {
   }
 } while (confirmJogadores !== 1);
 for (i = 0; i < quantidadeJogadores; i++) {
-    //criação de objeto para cada player
+  //criação de objeto para cada player
   var x = prompt(`Digite o nome do jogador ${i + 1}`);
   lista.push({
     Jogador: x,
@@ -39,7 +33,7 @@ for (i = 0; i < quantidadeJogadores; i++) {
     Derrotas: 0,
     Empate: 0,
     NumerosTirados: [],
-    RodadaAtual: 0
+    RodadaAtual: 0,
   });
 }
 //Implementação do game
@@ -49,37 +43,49 @@ do {
     var dadosJogador =
       valorDados[Math.floor(Math.random() * valorDados.length)];
     lista[j].NumerosTirados.push(dadosJogador);
-    lista[j].RodadaAtual =dadosJogador;
+    lista[j].RodadaAtual = dadosJogador;
+    console.log(`${lista[j].Jogador} tirou ${lista[j].RodadaAtual}`);
   }
-  lista.sort(function (a, b) {
-    if (a.RodadaAtual > b.RodadaAtual) {
+  lista
+    .sort(function (a, b) {
+      if (a.RodadaAtual > b.RodadaAtual) {
         return 1;
-    }
-    if (a.RodadaAtual == b.RodadaAtual) {
+      }
+      if (a.RodadaAtual == b.RodadaAtual) {
         return 1;
-    }
-    if (a.RodadaAtual < b.RodadaAtual) {
-      return -1;
-    }
-    return 0;
-  }
-  ).reverse();
- // console.log(lista)
-  if (lista[0].RodadaAtual == lista[1].RodadaAtual) {
+      }
+      if (a.RodadaAtual < b.RodadaAtual) {
+        return -1;
+      }
+      return 0;
+    })
+    .reverse();
+  // console.log(lista)
+  if (
+    lista[0].RodadaAtual == lista[1].RodadaAtual &&
+    lista[1].RodadaAtual == lista[2].RodadaAtual
+  ) {
+    lista[0].Empate++;
+    lista[1].Empate++;
+    lista[2].Empate++;
+    console.log(
+      `Empatou entre ${lista[0].Jogador}, ${lista[1].Jogador} e ${lista[2].Jogador}`
+    );
+  } else if (lista[0].RodadaAtual == lista[1].RodadaAtual) {
     lista[0].Empate++;
     lista[1].Empate++;
     console.log(`Empatou entre ${lista[0].Jogador} e ${lista[1].Jogador}`);
-  }else{
-      lista[0].Vitorias++;
-      console.log(`${lista[0].Jogador}`)
-  }   
+  } else {
+    lista[0].Vitorias++;
+    console.log(`O VENCEDOR É: ${lista[0].Jogador}`);
+  }
   k = k + 1;
 } while (quantidadeRodadas != k);
-//console.log(lista);
 // contabilizando as derrotas
-for (i = 0; i < lista.length; i++){ 
-    let y = quantidadeRodadas - (lista[i].Vitorias + lista[i].Empate)
-    lista[i].Derrotas = y
+for (i = 0; i < lista.length; i++) {
+  let y = quantidadeRodadas - (lista[i].Vitorias + lista[i].Empate);
+  lista[i].Derrotas = y;
+  delete lista[i].RodadaAtual;
 }
 //Ordenando a lista
 lista.sort(function (a, b) {
@@ -100,6 +106,6 @@ lista.sort(function (a, b) {
   return 0;
 });
 lista.reverse();
-console.log(`O grande vencedor é: ${lista[0].Jogador}`);
-console.log("Pódio");
+console.log(`\n O grande vencedor é: ${lista[0].Jogador}`);
+console.log("=============================\n              Pódio");
 console.log(lista);
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Blue
  Repositório para aulas e projetos da blue
+## Módulo 1 - Introdução a programação com JavaScript
+#### Projetos 
+1 - Detetive
+2 - Jokenpo
+3 - DiceGame
+
